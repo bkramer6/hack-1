0d798769cf65a739768105650a6cf3ec16f071e3
bkramer6 <42564252+bkramer6@users.noreply.github.com>
2018-08-23 15:57:44 -0500

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Matthew Schaffart and Brandon Schaffart Braxton kramer
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Braxton Kramer-computer science, Matthew Schaffart Computer Science, Brandon Schaffart - Computer Science\n");
+
+  return 0;
+}
